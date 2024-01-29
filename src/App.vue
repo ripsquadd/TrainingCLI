b3a3aa9aa1fc8c2ab15040301a11844699afa64c
angelina43 <bushueva.03@inbox.ru>
2024-01-30 02:02:34 +0700

    Декомпозиция формы и вывода

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <event-add @create="create_event"/>
+  <event-map :events="events"
+             @event_transfer_to_death="event_teleport_to_death"/>
 </template>
 
+<script>
+  import EventAdd from "@/components/EventAdd";
+  import EventMap from "@/components/EventMap";
+  export default {
+    name: "App",
+    components: {
+      EventAdd, EventMap
+    },
+    data () {
+      return {
+        events: [],
+        form_on : 0,
+      }
+    },
+    methods: {
+      create_event(new_event) {
+        this.events.push(new_event)
+        this.save();
+      },
+      add_form_show() {
+        this.form_on = 1;
+      },
+      add_form_hide() {
+        this.form_on = 0;
+      },
+      event_teleport_to_death(id) {
+        this.events.splice(id, 1);
+        this.save();
+      },
+      save() {
+        localStorage.event_map = JSON.stringify(this.events);
+      }
+    },
+    mounted() {
+      if (localStorage.event_map) {
+        this.events = JSON.parse(localStorage.event_map)
+      }if (localStorage.event_map) {
+        this.events = JSON.parse(localStorage.event_map)
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+  img {
+    width: 250px;
+    border: 5px solid black;
+  }
 </style>
